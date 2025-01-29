02a353ed2af3657e5c84d8ce88f7930619a4bf4b
SidoIndeedo <sidharths1001@gmail.com>
2025-01-29 15:31:42 +0530

    implemented sorting by input, description and if item is packed or not

--- src/App.js
+++ src/App.js
@@ -83,10 +83,23 @@ function Form({ onAddItems }) {
 }
 
 function PacakagingList({ item, onDeleteItem, onToogle }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItem;
+
+  if (sortBy === "input") sortedItem = item;
+
+  if (sortBy === "description")
+    sortedItem = item.slice().sort((a, b) => a.desc.localeCompare(b.desc));
+
+  if (sortBy === "packed")
+    sortedItem = item
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {item.map((item) => (
+        {sortedItem.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -95,6 +108,14 @@ function PacakagingList({ item, onDeleteItem, onToogle }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
